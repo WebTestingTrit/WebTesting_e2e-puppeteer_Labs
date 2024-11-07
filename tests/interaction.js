3efd474867da65d7545536475c46585a0e0484eb
Alberto Basalo <albertobasalo@hotmail.com>
2024-11-07 17:57:54 +0100

    fix: using duckduckgo instead of google

--- tests/interaction.js
+++ tests/interaction.js
@@ -1,15 +1,12 @@
 import { given, then, when } from '../lib/bit.tester.js';
 
 export default async function (pagePuppet) {
-  await given(`A term to find on google`, async () => {
-    const inputPageUrl = `https://www.google.com`;
+  await given(`A term to find on duckduckgo`, async () => {
+    const inputPageUrl = `https://www.duckduckgo.com`;
     const inputTerm = `aiddbot`;
     await pagePuppet.goto(inputPageUrl, { waitUntil: `networkidle2` });
     await when(`we search ${inputTerm}`, async () => {
       // Start of Selection
-      //await pagePuppet.focus('button div[role="none"]').click();
-      // accept cookies
-      await pagePuppet.keyboard.press('Enter');
       await pagePuppet.focus('input');
       await pagePuppet.keyboard.type(inputTerm);
       await pagePuppet.keyboard.press('Enter');
